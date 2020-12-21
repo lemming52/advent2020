9d35817d85c5b53b3321ee12182ff4053732f7e7
Andrew Strange <andrew@gumboot.co.uk>
2020-12-21 21:16:25 +0000

    Remove old test

--- solutions/dayseventeen/conway_cubes_test.go
+++ solutions/dayseventeen/conway_cubes_test.go
@@ -7,24 +7,6 @@ import (
 )
 
 func TestIterate(t *testing.T) {
-	init := [][]bool{
-		{false, true, false}, // .#.
-		{false, false, true}, // ..#
-		{true, true, true},   // ###
-	}
-	turn := 6
-	x, y, z := len(init[0])+turn*2, len(init[0])+turn*2, turn*2+1
-	offsetX, offsetY := turn, turn
-	expected := 112
-	c := NewCube(3, z, y, x)
-	c.Initalise(init, offsetX, offsetY)
-	for i := 0; i < turn; i++ {
-		c.Iterate()
-	}
-	assert.Equal(t, expected, c.Total())
-}
-
-func TestNIterate(t *testing.T) {
 	turn := 6
 	init := [][]bool{
 		{false, true, false}, // .#.
